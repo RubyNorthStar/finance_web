8a440a111246a4c1681a862d68988aee31b9dfda
wangzhixing <wang.zx1@yougou.com>
2014-09-29 18:02:25 +0800

    添加数据库表结构

--- doc/update.sql
+++ doc/update.sql
@@ -129,3 +129,79 @@ CREATE TABLE `tbl_users` (
 -- ----------------------------
 -- Records of tbl_users
 -- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for `tbl_product_info` (产品详情表)
+-- ----------------------------
+
+CREATE TABLE `tbl_product_info` (
+	`id`  int(11) NOT NULL AUTO_INCREMENT ,
+	`product_name`  varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目名称' ,
+	`fund_scale`  varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基金规模' ,
+	`expect_profit`  varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '预期收益' ,
+	`fund_type_id`  int(11) NULL DEFAULT NULL COMMENT '基金类型( 基金类型表ID )' ,
+	`investment_time_limit`  tinyint(2) NULL DEFAULT NULL COMMENT '投资期限( 以月为单位 )' ,
+	`fund_manage`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基金管理( 目前理解为公司名称 )' ,
+	`min_bid_money`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '起投金额( 目前为字符串，如果单位定了的话，转为int类型 )' ,
+	`profit_distribution`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收益分配 ( 同起投金额 )' ,
+	`begin_time`  datetime NULL DEFAULT NULL COMMENT '产品投资开始时间' ,
+	`end_time`  datetime NULL DEFAULT NULL COMMENT '产品投资结束时间( 根据开始时间及募集日期推算 )' ,
+	`product_detaile_info`  text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '项目介绍' ,
+	`is_mortgage`  tinyint(2) NULL DEFAULT NULL COMMENT '是否抵押( 1:有抵押，2:未抵押 )' ,
+	`fund_use`  text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '资金用途' ,
+	`repayment_source`  text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '还款方式' ,
+	`business_structure`  text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT 'business_structure' ,
+	`wind_control_measures`  text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '风控措施' ,
+	`to_examine_status`  tinyint(2) NULL DEFAULT NULL COMMENT '审核状态( 1.未审核 2.审核)' ,
+	`product_mortgage_id`  int(11) NULL DEFAULT NULL COMMENT '产品抵押物品(对应抵押物品表)' ,
+	PRIMARY KEY (`id`)
+	)
+	ENGINE=InnoDB
+	DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+	AUTO_INCREMENT=1
+	CHECKSUM=0
+	ROW_FORMAT=DYNAMIC
+	DELAY_KEY_WRITE=0
+;
+
+
+-- ----------------------------
+-- Table structure for `tbl_product_mortgage` (产品对应抵押物品表)
+-- ----------------------------
+
+CREATE TABLE `tbl_product_mortgage` (
+`id`  int(11) NOT NULL ,
+`mortgage_name`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '抵押物品名称' ,
+`imag_url`  varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '抵押物品对应图片' ,
+`product_id`  int(11) NOT NULL COMMENT '对应产品ID' ,
+`status`  tinyint(2) NULL DEFAULT NULL COMMENT '1.可用 2.不可用' ,
+`create_time`  datetime NULL DEFAULT NULL ,
+PRIMARY KEY (`id`)
+)
+ENGINE=MyISAM
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+CHECKSUM=0
+ROW_FORMAT=DYNAMIC
+DELAY_KEY_WRITE=0
+;
+
+-- ----------------------------
+-- Table structure for `tbl_product_fund_type` (基金类型表)
+-- ----------------------------
+
+CREATE TABLE `tbl_product_fund_type` (
+`id`  int(11) NOT NULL AUTO_INCREMENT ,
+`investment_name`  varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '投资类型名称' ,
+`status`  tinyint(2) NULL DEFAULT 1 COMMENT '状态： 1 可用，2不可用' ,
+`create_time`  datetime NULL DEFAULT NULL ,
+PRIMARY KEY (`id`)
+)
+ENGINE=MyISAM
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1
+CHECKSUM=0
+ROW_FORMAT=DYNAMIC
+DELAY_KEY_WRITE=0
+;
+
