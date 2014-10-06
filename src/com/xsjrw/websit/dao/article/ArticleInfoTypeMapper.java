2f520ec8d9e8ee56fd57874c612ef21491f10857
wangzhixing <wang.zx1@yougou.com>
2014-10-06 17:15:11 +0800

    修改文章

--- src/com/xsjrw/websit/dao/article/ArticleInfoTypeMapper.java
+++ src/com/xsjrw/websit/dao/article/ArticleInfoTypeMapper.java
@@ -1,8 +1,8 @@
 package com.xsjrw.websit.dao.article;
 
 import com.xsjrw.websit.core.domain.BaseMapper;
-import com.xsjrw.websit.domain.article.ArticleInfo;
+import com.xsjrw.websit.domain.article.ArticleInfoType;
 
-public interface ArticleInfoTypeMapper extends BaseMapper<ArticleInfo>{
+public interface ArticleInfoTypeMapper extends BaseMapper<ArticleInfoType>{
 
 }
