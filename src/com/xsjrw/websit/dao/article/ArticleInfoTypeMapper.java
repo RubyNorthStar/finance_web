8a3d5209c8b337284077afe03d501723e079824c
topkaiser <wanghao-888666@163.com>
2014-10-06 17:55:10 +0800

    4

--- src/com/xsjrw/websit/dao/article/ArticleInfoTypeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.xsjrw.websit.dao.article;
-
-import com.xsjrw.websit.core.domain.BaseMapper;
-import com.xsjrw.websit.domain.article.ArticleInfoType;
-
-public interface ArticleInfoTypeMapper extends BaseMapper<ArticleInfoType>{
-
-}
